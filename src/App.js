cf989d3ed941455d07174eb9dc9778eca8436f45
ulissestb <ulisses.melo.nascimento@gmail.com>
2018-10-07 20:42:41 -0300

    adicionando busca no app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,6 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
 
-const contacts = [
-  {
-    "id": "karen",
-    "name": "Karen Isgrigg",
-    "handle": "karen_isgrigg",
-    "avatarURL": "http://localhost:5001/karen.jpg"
-  },
-  {
-    "id": "richard",
-    "name": "Richard Kalehoff",
-    "handle": "richardkalehoff",
-    "avatarURL": "http://localhost:5001/richard.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "handle": "tylermcginnis",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
- ];
 
 class App extends Component {
   state = {
